62cfea69c8a7f2187dd29b0236e2c747a5b21ac5
Pooj0605 <kanna.p@northeastern.edu>
2025-05-07 14:59:13 -0400

    mades changes in react1

--- neu-chatbot/src/App.js
+++ neu-chatbot/src/App.js
@@ -483,7 +483,7 @@ function App({ user, onLogout }) {
         "Can you suggest a good study spot on campus for Monday at 2 PM where I can stay for a few hours?",
         "What are my options for finding a study spot in Churchill Hall on Monday afternoon?",
         "International Village 018 available for study for one hour on Monday ?",
-        "Can you give me the class schedule for Room 229 in Richards 229 Hall on Monday?"
+        "Can you give me the class schedule for Room 229 in Richards Hall on Monday?"
       ];
     } else {
       return [
